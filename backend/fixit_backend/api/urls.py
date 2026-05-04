ae7234298a7a5f51e56af502f74ed7755d6ae428
ITSROHITTECH <srivastavarohitkumar067@gmail.com>
2026-05-04 18:56:14 +0530

    .

--- backend/fixit_backend/api/urls.py
+++ backend/fixit_backend/api/urls.py
@@ -3,12 +3,12 @@ from .views import test_api, signup, login_api,contact_api,create_booking,my_boo
 
 
 urlpatterns = [
-    path('/test/', test_api),
-    path('/signup/', signup),
-    path('/login/',login_api),
-    path('/contact/', contact_api),
-    path('/booking/', create_booking),
-    path("/my-bookings/", my_bookings),
+    path('test/', test_api),
+    path('signup/', signup),
+    path('login/',login_api),
+    path('contact/', contact_api),
+    path('booking/', create_booking),
+    path("my-bookings/", my_bookings),
     
 ]
 
